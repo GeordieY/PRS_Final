ac63a9e894d90bafa6583c355f3d59a48d7b681c
Mit Ramesh <mithun.ramesh19@trinityschoolnyc.org>
2019-02-05 09:29:57 -0500

    add

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var Users = require('../models/User');
 var Villains = require('../models/Villain')
 
-router.post('/user/:id', function(req, res){
+router.put('/user/:id', function(req, res){
   console.log('POST Request- /user/'+req.params.id);
 
   var u={
@@ -16,13 +16,24 @@ router.post('/user/:id', function(req, res){
   res.render('user_details', {user:u});
 });
 
+router.post('/users',function(req,res){
+  res.status(200);
+  res.setHeader('Content-Type', 'text/html');
+  res.render('index', {user:u});
+});
 router.get('/user/new', function(req,res){
   var userinfo = "";
   res.status(200);
-  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Content-Type', 'text/html');
   res.render('user_details', {user:userinfo});
 });
 
+router.delete('/users:id', function(req,res) {
+  var id = req.params.user;
+  res.status(200);
+  res.setHeader('Content-Type', 'text/html');
+  res.render('user_details'. {user:id});
+});
 
 
 
