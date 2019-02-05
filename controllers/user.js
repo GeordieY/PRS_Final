a926ef3dec7f8a68af901008c84a6dbea98af7c6
Mit Ramesh <mithun.ramesh19@trinityschoolnyc.org>
2019-02-05 09:19:01 -0500

    changingcontroller

--- controllers/user.js
+++ controllers/user.js
@@ -2,17 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 var Users = require('../models/User');
+var Villains = require('../models/Villain')
 
-router.get('/user/:id', function(req, res){
-  console.log('Request- /user/'+req.params.id);
+router.post('/user/:id', function(req, res){
+  console.log('POST Request- /user/'+req.params.id);
 
-  var u = Users.getUser(req.params.id);
+  var u={
+    name = req.body.username.trim(),
+    password=req.body.password.trim()
+  }
   res.status(200);
   res.setHeader('Content-Type', 'text/html')
   res.render('user_details', {user:u});
 });
 
-router.get('/user/new')
+router.get('/user/new', function(req,res){
+  var userinfo = "";
+  res.status(200);
+  res.setHeader('Content-Type', 'text/html')
+  res.render('user_details', {user:userinfo});
+});
 
 
 
