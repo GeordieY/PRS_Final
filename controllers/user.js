50f1c094b739616e2f8fcf7671309164aaa50ecc
Mit Ramesh <mithun.ramesh19@trinityschoolnyc.org>
2019-02-07 12:29:17 -0500

    controller

--- controllers/user.js
+++ controllers/user.js
@@ -2,8 +2,56 @@ var express = require('express');
 var router = express.Router();
 
 var Users = require('../models/User');
-var Villains = require('../models/Villain')
+var Villains = require('../models/Villain');
+var Game = requre('../models/Game');
 
+//getting a new user which loads a blank form
+router.get('/user/new', function(req,res){
+var u = {}
+res.status(200);
+res.setHeader('Content-Type', 'text/html');
+res.render('user_details', {user:u});
+});
+
+
+//how to get the userID and render the user details
+router.get('/user/:id', function(req,res){
+  var u={
+    name = req.params.id,
+    password = req.params.password
+  }
+  res.status(200);
+  res.setHeader('Content-Type', 'text/html')
+  res.render('user_details', {user:u});
+});
+
+//method to add a new user and return to the game page
+router.post('/users', function(req,res){
+  var u = [];
+  var name = req.body.username.trim();
+  var password=req.body.password.trim();
+  u.push(name);
+  u.push(password);
+  Users.createUser(u);
+  res.redirect('/');
+});
+
+//get the user you have and load the information and then load the edit screen
+router.get('/users/:id/edit', function(req,res){
+  var name = req.params.id;
+  var user = Users.getUser(name);
+  res.status(200);
+  res.setHeader('Content-Type', 'text/html')
+  res.render('user_details', {user:user});
+});
+
+router.put('/users/:id', function(req,res){
+  var name = req.params.id;
+});
+
+
+
+/*
 router.put('/user/:id', function(req, res){
   console.log('POST Request- /user/'+req.params.id);
 
@@ -34,7 +82,7 @@ router.delete('/users:id', function(req,res) {
   res.setHeader('Content-Type', 'text/html');
   res.render('user_details'. {user:id});
 });
-
+*/
 
 
 module.exports = router;
